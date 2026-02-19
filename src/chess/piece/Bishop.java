d845bc10b4922abb5900c3efbc39472b5624a594
Shreyas Jadhav <shreyasajadhav123@gmail.com>
2026-02-19 22:35:07 +0530

    Moves detected

--- src/chess/piece/Bishop.java
+++ src/chess/piece/Bishop.java
@@ -5,6 +5,7 @@ import chess.model.PieceColor;
 import chess.model.PieceType;
 import chess.model.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bishop extends Piece{
@@ -22,7 +23,36 @@ public class Bishop extends Piece{
 
     @Override
     public List<Position> getPossibleMoves(Board board, Position from) {
-        return List.of();
+        List<Position> moves = new ArrayList<>();
+
+        int[][] directions = {
+                {-1,-1}, // NW
+                {-1, 1}, // NE
+                {1, -1}, // SW
+                {1 , 1}  // SE
+        };
+
+        for(int[] dir : directions) {
+            int dr = dir[0];
+            int dc = dir[1];
+            int newRow = from.getRow() + dr;
+            int newCol = from.getCol() + dc;
+
+            while(newRow >= 0 && newRow <=7 && newCol >= 0 && newCol <= 7){
+                Position to = new Position(newRow, newCol);
+                if(board.getPiece(to) == null) {
+                    moves.add(to);
+                } else {
+                    if(board.getPiece(to).getColor() != getColor()) {
+                        moves.add(to);
+                    }
+                    break;
+                }
+                newRow += dr;
+                newCol += dc;
+            }
+        }
+        return moves;
     }
 
 }
--- src/chess/piece/Queen.java
+++ src/chess/piece/Queen.java
@@ -5,6 +5,7 @@ import chess.model.PieceColor;
 import chess.model.PieceType;
 import chess.model.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Queen extends Piece{
@@ -23,6 +24,31 @@ public class Queen extends Piece{
 
     @Override
     public List<Position> getPossibleMoves(Board board, Position from) {
-        return List.of();
+        List<Position> moves = new ArrayList<>();
+        int[] directions = {-1, 0, 1};
+
+        for(int dr : directions){
+            for(int dc : directions){
+                if(dr == 0 || dc == 0){
+                    continue;
+                }
+                int newRow = from.getRow() + dr;
+                int newCol = from.getCol() + dc;
+                while(newRow >= 0 && newRow <= 7 && newCol >= 0 && newCol <= 7){
+                    Position to = new Position(newRow, newCol);
+                    if(board.getPiece(to) == null) {
+                        moves.add(to);
+                    } else {
+                        if(board.getPiece(to).getColor() != getColor()) {
+                            moves.add(to);
+                        }
+                        break;
+                    }
+                    newRow += dr;
+                    newCol += dc;
+                }
+            }
+        }
+        return moves;
     }
 }
